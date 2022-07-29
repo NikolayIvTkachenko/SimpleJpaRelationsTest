6b03ed745b9b031807bbd8dd5959b4a9c46eeb8e
NikolayIT <tkachenni@mail.ru>
2022-07-29 12:36:44 +0300

    add all relations working

--- src/main/java/com/rsh/testrelationtable/trt01/entity/Customer.java
+++ src/main/java/com/rsh/testrelationtable/trt01/entity/Customer.java
@@ -1,13 +1,22 @@
 package com.rsh.testrelationtable.trt01.entity;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+
 
 @Entity
 @Table
@@ -20,6 +29,23 @@ public class Customer {
 	private String name;
 	
 	
+	//ManyToMany
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "customer_item",
+			joinColumns = {@JoinColumn(name = "customer_fk")},
+			inverseJoinColumns = {@JoinColumn(name = "item_fk")})
+	private Set<Item> itemsManyToMany = new HashSet<Item>();
+	
+	
+	
+	//OneToMany
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "customerOneTMany")	
+	//@JoinColumn(name = "customer_id")
+	private List<Item> itemsOneToMany = new ArrayList<>();
+	
+	
+	
+	//OneToOne
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "fk_item")
 //	@JoinTable(name = "customer_item",
@@ -54,6 +80,23 @@ public class Customer {
 	public Long getId() {
 		return id;
 	}
+
+	public List<Item> getItemsOneToMany() {
+		return itemsOneToMany;
+	}
+
+	public void setItemsOneToMany(List<Item> itemsOneToMany) {
+		this.itemsOneToMany = itemsOneToMany;
+	}
+
+	public Set<Item> getItemsManyToMany() {
+		return itemsManyToMany;
+	}
+
+	public void setItemsManyToMany(Set<Item> itemsManyToMany) {
+		this.itemsManyToMany = itemsManyToMany;
+	}
+	
 	
 	
 
--- src/main/java/com/rsh/testrelationtable/trt01/entity/Item.java
+++ src/main/java/com/rsh/testrelationtable/trt01/entity/Item.java
@@ -1,9 +1,16 @@
 package com.rsh.testrelationtable.trt01.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -24,6 +31,18 @@ public class Item {
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "item")
 	private Customer customer;
 	
+	
+	@JsonIgnore
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "customer_id")
+	private Customer customerOneTMany;
+	
+	
+	@JsonIgnore
+	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "itemsManyToMany")
+	private Set<Customer> customerManyToMany = new HashSet<Customer>();
+	
+	
 	public Item() {}
 	
 	public Item(String name) {
@@ -40,6 +59,14 @@ public class Item {
 
 	public Long getId() {
 		return id;
+	}
+
+	public Set<Customer> getCustomerManyToMany() {
+		return customerManyToMany;
+	}
+
+	public void setCustomerManyToMany(Set<Customer> customerManyToMany) {
+		this.customerManyToMany = customerManyToMany;
 	}
 	
 	
